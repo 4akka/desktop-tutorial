c0ccd5347ba5c1c1e8de90e55a7a963e7719cd4f
ChakkaMan <studenakdenis@gmail.com>
2026-01-23 18:39:08 +0200

    Моя Анкета

---
+++ Анкета.py
@@ -0,0 +1,21 @@
+print("=== АНКЕТА КОРИСТУВАЧА ===")
+
+name = "Денис"
+surname = "Студеняк"
+age = 15
+city = "Київ"
+country = "Україна"
+height = 173      # см
+weight = 64       # кг
+study = True
+hobby = "Кунг-фу"
+sport = True
+
+print("Ім'я:", name)
+print("Прізвище:", surname)
+print("Вік:", age)
+print("Місто:", city)
+print("Країна:", country)
+print("Зріст:", height, "см")
+print("Вага:", weight, "кг")
+print("Хобі:", hobby)
